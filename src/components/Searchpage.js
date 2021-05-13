9291885357c328e2ad7560cbfc4350e5af86e90e
keshavsharmalws <keshav@ladybirdweb.com>
2021-05-13 10:59:39 +0530

    delete romms filter

--- src/components/Searchpage.js
+++ src/components/Searchpage.js
@@ -14,7 +14,6 @@ export default function Searchpage() {
               <Button variant='outlined'>Cancellation Flexibility </Button>
               <Button variant='outlined'>Type of place </Button>
               <Button variant='outlined'>Price </Button>
-              <Button variant='outlined'> Rooms & Beds</Button>
               <Button variant='outlined'>More filters </Button>
           </div>
           <Searchresult img={room1}
