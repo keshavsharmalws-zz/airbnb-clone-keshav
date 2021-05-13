605b5c93d8e29fd4cee7ffaa09701528959db851
keshavsharmalws <keshav@ladybirdweb.com>
2021-05-13 11:06:00 +0530

    deleted filters

--- src/components/Searchpage.js
+++ src/components/Searchpage.js
@@ -14,7 +14,7 @@ export default function Searchpage() {
               <Button variant='outlined'>Cancellation Flexibility </Button>
               <Button variant='outlined'>Type of place </Button>
               <Button variant='outlined'>Price </Button>
-              <Button variant='outlined'>More filters </Button>
+            
           </div>
           <Searchresult img={room1}
            location="private room in london" 
